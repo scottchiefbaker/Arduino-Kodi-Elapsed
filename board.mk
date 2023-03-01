855f6ca836279281959ec6e79bd160acb73e650f
Scott Baker <scott@perturb.org>
2023-03-01 11:11:31 -0800

    Don't need a board.mk file anymore

--- board.mk
+++
@@ -1,7 +0,0 @@
-#BOARD = esp8266:esp8266:nodemcuv2:baud=460800                          # NodeMCU/ESP8266
-#BOARD = esp8266:esp8266:d1_mini:baud=921600                            # Wemos D1 Mini
-#BOARD = esp32:esp32:esp32:CPUFreq=240,FlashMode=qio,UploadSpeed=921600 # ESP32
-#BOARD = arduino:avr:nano:cpu=atmega168
-
-BOARD = arduino:avr:nano:cpu=atmega328old
-PORT  = /dev/ttyUSB0
